0822a4438e69145c79d054bb4557fd779441f341
Deepak Garg <dgargkyt@gmail.com>
2023-06-06 22:10:50 +0530

    First Commit

---
+++ Guest-Microservice/src/main/java/com/microservices/guest/GuestMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.guest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestMicroserviceApplication.class, args);
+	}
+
+}
