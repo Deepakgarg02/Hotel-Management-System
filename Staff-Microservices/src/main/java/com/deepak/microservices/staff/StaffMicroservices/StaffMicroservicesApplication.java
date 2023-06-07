b1176f0311325db72d7cf37c474cc1b8c171e4a9
Deepak Garg <dgargkyt@gmail.com>
2023-06-07 10:43:58 +0530

    Staff Microservice Initial Stage

---
+++ Staff-Microservices/src/main/java/com/deepak/microservices/staff/StaffMicroservices/StaffMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.microservices.staff.StaffMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffMicroservicesApplication.class, args);
+	}
+
+}
---
+++ Staff-Microservices/src/test/java/com/deepak/microservices/staff/StaffMicroservices/StaffMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.microservices.staff.StaffMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
