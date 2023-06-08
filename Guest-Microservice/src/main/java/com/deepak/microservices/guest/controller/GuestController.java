96ed19e03564219f26a6c4b98497ce4054ff3383
Deepak Garg <dgargkyt@gmail.com>
2023-06-08 10:18:29 +0530

    Lombok Applied to Guest Service

--- Guest-Microservice/src/main/java/com/deepak/microservices/guest/controller/GuestController.java
+++ Guest-Microservice/src/main/java/com/deepak/microservices/guest/controller/GuestController.java
@@ -30,8 +30,13 @@ public class GuestController {
 
 	@PostMapping("/add")
 	public String addGuest(@RequestBody Guest guest) {
+		try {
 		guestServiceImpl.addGuest(guest);
 		return "Guest Added with guestId " + guest.getGuestId();
+		}
+		catch(Exception e) {
+			return "Use Proper Input";
+		}
 	}
 
 	@PutMapping("/modify/{guestId}")
