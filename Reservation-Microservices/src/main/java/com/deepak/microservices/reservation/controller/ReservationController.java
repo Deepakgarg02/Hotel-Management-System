d0692f6271e4275b5e3b749de7f9ea220db0c662
Deepak Garg <dgargkyt@gmail.com>
2023-06-10 16:59:32 +0530

    Added Fallback Methods in this

--- Reservation-Microservices/src/main/java/com/deepak/microservices/reservation/controller/ReservationController.java
+++ Reservation-Microservices/src/main/java/com/deepak/microservices/reservation/controller/ReservationController.java
@@ -2,19 +2,13 @@ package com.deepak.microservices.reservation.controller;
 
 import java.util.List;
 import java.util.Optional;
-
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import com.deepak.microservices.reservation.exception.InvalidReservationIdException;
 import com.deepak.microservices.reservation.model.Reservation;
 import com.deepak.microservices.reservation.service.ReservationService;
@@ -28,6 +22,8 @@ public class ReservationController {
 	@Autowired
 	private ReservationService reservationService;
 
+	private Logger logger = LoggerFactory.getLogger(ReservationController.class);
+
 	@PostMapping("/add")
 	@ApiOperation(value = "Adding Reservation details in DB")
 	public String addReservation(@RequestBody Reservation reservation) {
@@ -36,13 +32,31 @@ public class ReservationController {
 
 	@GetMapping("/get")
 	@ApiOperation(value = "Getting all Reservation Details From DB")
+	@RateLimiter(name = "reservationRateLimiter", fallbackMethod = "allReservationsFallback")
 	public ResponseEntity<List<Reservation>> getAllReservations() {
+		logger.info("Get All Reservations Handler: ReservationController");
 		return ResponseEntity.ok(reservationService.gettAllReservations());
+
 	}
 
+	// Creating fallback method for rate limiter - getAllReservations
+	public ResponseEntity<List<Reservation>> allReservationsFallback(Exception e) {
+		e.printStackTrace();
+
+		Reservation fallbackReservation = Reservation.builder().checkInDate("2021-05-10").checkOutDate("2021-05-20")
+				.roomId("111222335").guestId("111222335").reservationId("111222335").numOfGuest(2).totalPrice(0.0)
+				.build();
+
+		return ResponseEntity.ok(List.of(fallbackReservation));
+	}
+
+	// Providing RateLimiter with FallBack Method
 	@GetMapping("/get/{resId}")
 	@ApiOperation(value = "Getting Reservation Details By Id")
+	@RateLimiter(name = "reservationRateLimiter", fallbackMethod = "guestRoomFallback")
 	public ResponseEntity<Optional<Reservation>> getReservationById(@PathVariable String resId) {
+
+		logger.info("Get Single Reservation Handler: ReservationController");
 		try {
 			Optional<Reservation> reservation = reservationService.getReservationById(resId);
 			if (reservation.isPresent()) {
@@ -54,6 +68,17 @@ public class ReservationController {
 			// TODO: handle exception
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
+	}
+
+	// Creating fallback method for rate limiter
+	public ResponseEntity<Optional<Reservation>> guestRoomFallback(Exception e) {
+		e.printStackTrace();
+
+		Reservation fallbackReservation = Reservation.builder().checkInDate("2021-05-10").checkOutDate("2021-05-20")
+				.roomId("111222335").guestId("111222335").reservationId("111222335").numOfGuest(2).totalPrice(0.0)
+				.build();
+
+		return ResponseEntity.ok(Optional.of(fallbackReservation));
 	}
 
 	@PutMapping("/modify/{resId}")
--- Reservation-Microservices/src/main/java/com/deepak/microservices/reservation/model/Reservation.java
+++ Reservation-Microservices/src/main/java/com/deepak/microservices/reservation/model/Reservation.java
@@ -15,6 +15,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Builder
 @Entity
 @Table(name = "reservationmicroservice")
 public class Reservation {
